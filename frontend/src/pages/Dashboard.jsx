c8507c6fc470de59edfea3f41a54355073ffea55
Yi Zhong <yi.zhong-5@student.uts.edu.au>
2026-04-05 17:27:18 +1000

    Header and Dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <Header />
+
+      <main className="dashboard-content">
+        <section className="placeholder-section">
+          <h2>Welcome to PocketGenie</h2>
+          <p>Add your transactions below to keep track of your income and expenses.  </p>
+          <p>Stay organized and understand your spending at a glance.</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Dashboard;
